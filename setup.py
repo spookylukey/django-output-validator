264854f40be6b23532d9a50b4bf17bdee30b2536
Luke Plant <L.Plant.98@cantab.net>
2010-12-01 21:52:02 +0000

    Fixed setup.py typo

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Framework :: Django",
